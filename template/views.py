641a2d4b93c10d968d7ad25fc9cd348dc2eeef43
Alphonce <alphoncerotich30@gmail.com>
2023-10-08 10:02:02 +0300

    'myproject'

--- template/views.py
+++ template/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from .models import Fashion
-import os
-from django.conf import settings
-from .models import Images,Product
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'index.html')
